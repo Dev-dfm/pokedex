0261d378ea7bf7af45e781ca74701c0a0bd78a36
Dev-dfm <d.floresmedina@gmail.com>
2021-05-09 06:43:58 +0200

    ➕ Add Link to single character page

--- components/character.js
+++ components/character.js
@@ -3,24 +3,30 @@ import { createElement } from '../utils/elements';
 
 export function createCharacterElement({ name, url }) {
   const pokemonId = url.match(/(\d+)(?!.*\d)/g);
-  return createElement('div', {
-    className: 'character-card',
+  return createElement('a', {
+    className: 'character-link',
+    href: `/details.html?id=1`,
     children: [
-      createElement('img', {
-        className: 'character-card__image',
-        src: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`,
-      }),
       createElement('div', {
-        className: 'character-card__info',
+        className: 'character-card',
         children: [
+          createElement('img', {
+            className: 'character-card__image',
+            src: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`,
+          }),
           createElement('div', {
-            className: 'character-card__name',
+            className: 'character-card__info',
             children: [
-              createElement('h2', {
-                innerText: name,
-              }),
-              createElement('h2', {
-                innerText: `#${pokemonId}`,
+              createElement('div', {
+                className: 'character-card__name',
+                children: [
+                  createElement('h2', {
+                    innerText: name,
+                  }),
+                  createElement('h2', {
+                    innerText: `#${pokemonId}`,
+                  }),
+                ],
               }),
             ],
           }),
